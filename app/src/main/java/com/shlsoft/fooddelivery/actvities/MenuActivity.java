4cbdf648114d816f88e4cb95ccc71b67f49fd77a
shaxzodshox <shaxzod-original@mail.ru>
2020-04-27 18:32:23 +0500

    Implemented navigation drawer and floating action button

--- app/src/main/java/com/shlsoft/fooddelivery/actvities/MenuActivity.java
+++ app/src/main/java/com/shlsoft/fooddelivery/actvities/MenuActivity.java
@@ -1,19 +1,51 @@
 package com.shlsoft.fooddelivery.actvities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
 import com.shlsoft.fooddelivery.R;
+import com.shlsoft.fooddelivery.fragment.CartFragment;
+import com.shlsoft.fooddelivery.fragment.MenuFragment;
+import com.shlsoft.fooddelivery.fragment.OrderFragment;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        loadFragment(new MenuFragment());
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,
+                R.string.open,R.string.close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     private void showExitDialog() {
@@ -36,5 +68,32 @@ public class MenuActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         showExitDialog();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_menu:
+                loadFragment(new MenuFragment());
+                break;
+            case R.id.nav_cart:
+                loadFragment(new CartFragment());
+                break;
+            case R.id.nav_orders:
+                loadFragment(new OrderFragment());
+                break;
+            case R.id.nav_sign_out:
+                // TODO: 27.04.2020 implement sign out
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void loadFragment(Fragment fragment){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.frame_container,fragment);
+        transaction.commit();
     }
 }
